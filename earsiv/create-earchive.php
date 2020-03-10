e7ebb6068b47f0987f54fc65ea483023aa098d65
algoritma <aliihsanbayir@gmail.com>
2020-03-10 13:20:39 +0300

    Update create-earchive.php

--- earsiv/create-earchive.php
+++ earsiv/create-earchive.php
@@ -1,41 +1,66 @@
 <?php
-	ini_set("soap.wsdl_cache_enabled", "0"); // wsdl cache 'ini devre disi birak
-	$Username = 'izibiz-test1';
-	$Password = 'izi321';
+if (!ini_get('display_errors')) ini_set('display_errors', 1);
+if (!ini_get('display_startup_errors')) ini_set('display_startup_errors', 'on');
+// Tüm hataları görelim
+error_reporting(E_ALL);
 
-	$client = new SoapClient("https://efaturatest.izibiz.com.tr/EFaturaOIB?wsdl");
+    include '../servisLinkleri.php';
+
+	ini_set("soap.wsdl_cache_enabled", "0"); // wsdl cache 'ini devre disi birak
+
+	$Username = 'usertest';
+	$Password = 'test123';
+
+	$client = new SoapClient($authentication);
 try {
-	$Req["USER_NAME"] =	"izibiz-test2";
-	$Req["PASSWORD"] = "izi321";
+	$Req["USER_NAME"] =	$Username;
+	$Req["PASSWORD"] = $Password;
 	$RequestHeader["SESSION_ID"] = "-1";
 	$Req["REQUEST_HEADER"] = $RequestHeader;
 	$Res = $client->Login($Req);
-	$archiveClient = new SoapClient("https://efaturatest.izibiz.com.tr/EIArchiveWS/EFaturaArchive?wsdl");
+	$archiveClient = new SoapClient($eArsiv);
 
-	$content = file_get_contents('earsivxml.php');
+	//$content = file_get_contents('earsivxml.php');
+	require_once 'earsivxml.php';
 
 	$Request = array(
-		"REQUEST_HEADER"	=>	array(
-			"SESSION_ID"	=>	$Res->SESSION_ID,
-			"COMPRESSED"	=>	"N"
+			"REQUEST_HEADER"	=>	array(
+			"SESSION_ID"		=>	$Res->SESSION_ID,
+			"COMPRESSED"		=>	"N"
 		),
 		"ArchiveInvoiceExtendedContent"	=>	array(
-			"INVOICE_PROPERTIES"	=>	array(
-				"EARSIV_FLAG"	=>	"Y",
-				"EARSIV_PROPERTIES"	=>	array(
-					"EARSIV_TYPE"	=>	"NORMAL",
-					"EARSIV_EMAIL_FLAG"	=>	"Y",
-					"EARSIV_EMAIL"	=>	"entegrasyon@izibiz.com.tr",
-					"SUB_STATUS"	=>	"NEW"
-				),
-				"INVOICE_CONTENT"	=>	$content
+			"INVOICE_PROPERTIES"=>	array(
+			"EARSIV_FLAG"		=>	"Y",
+			"EARSIV_PROPERTIES"	=>	array(
+				"EARSIV_TYPE"		=>	"NORMAL",
+				"EARSIV_EMAIL_FLAG"	=>	"Y",
+				"EARSIV_EMAIL"		=>	"entegrasyon@izibiz.com.tr",
+				"SUB_STATUS"		=>	"NEW"
+			),
+			"INVOICE_CONTENT"	=>	$content
 			)
 		)
 	);
 
 	print_r($Request);
-	$sendF = $archiveClient->WriteToArchiveExtended($Request);
-	print_r($sendF);
+	echo "<br>------------------------------------------<br>";
+	$response = $archiveClient->WriteToArchiveExtended($Request);
+		$res_transaction_ID = $response->ERROR_TYPE->INTL_TXN_ID;
+		$res_err_code 		= $response->ERROR_TYPE->ERROR_CODE;
+		$res_err_desc 		= $response->ERROR_TYPE->ERROR_SHORT_DES;
+
+	if ($res_err_code == -1) {
+		echo "işlem BAŞARISIZ<br>";
+	} else {
+		echo "OK<br>";
+	}
+	echo $res_transaction_ID ."<br>";
+	echo $res_err_code ."<br>";
+	echo $res_err_desc ."<br>";	
+	echo "<br>------------------------------------------<br>";
+
+
+	print_r($response);
 } catch (Exception $exc) { // Hata olusursa yakala
   // Son istegi ekrana bas
   echo "Son yapilan istek asagidadir<br/><pre>";
