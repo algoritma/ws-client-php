209678f6ca5b9e1c8ec164ad7aa576f50b3e70a0
algoritma <aliihsanbayir@gmail.com>
2020-03-10 12:04:49 +0300

    Update servisLinkleri.php

--- servisLinkleri.php
+++ servisLinkleri.php
@@ -1,20 +1,32 @@
 <?php
-error_reporting(1);
+error_reporting(0);
+
+/*
+$eArsiv = 'https://efaturadev.izibiz.com.tr:443/EIArchiveWS/EFaturaArchive?wsdl';
 
 $eFatura = 'https://efaturadev.izibiz.com.tr:443/EFaturaOIB?wsdl';
 
-$eArsiv = 'https://efaturadev.izibiz.com.tr:443/EIArchiveWS/EFaturaArchive?wsdl';
-
 $eDespatch = 'https://efaturadev.izibiz.com.tr:443/EIrsaliyeWS/EIrsaliye?wsdl';
-$eIrsaliye = 'https://efaturadev.izibiz.com.tr:443/EIrsaliyeWS/EIrsaliye?wsdl';
+
+$eSmm = 'https://efaturadev.izibiz.com.tr:443/SmmWS?wsdl';
+
+$auth = 'https://efaturatest.izibiz.com.tr:443/AuthenticationWS?wsdl';
+*/
+
+$eFatura = 'https://efaturatest.izibiz.com.tr/EFaturaOIB?wsdl';
+
+$eArsiv = 'https://efaturatest.izibiz.com.tr/EIArchiveWS/EFaturaArchive?wsdl';
+
+$eDespatch = 'https://efaturatest.izibiz.com.tr/EIrsaliyeWS/EIrsaliye?wsdl';
+$eIrsaliye = 'https://efaturatest.izibiz.com.tr/EIrsaliyeWS/EIrsaliye?wsdl';
 
 $eMustahsil = 'http://efaturatest.izibiz.com.tr/CreditNoteWS/CreditNote?wsdl';
 
 $eMutabakat = 'http://efaturatest.izibiz.com.tr/Reconciliation?wsdl';
 
-$eSmm = 'https://efaturadev.izibiz.com.tr:443/SmmWS?wsdl';
+$eSmm = 'https://efaturatest.izibiz.com.tr/SmmWS?wsdl';
 
-$auth = 'https://efaturatest.izibiz.com.tr:443/AuthenticationWS?wsdl';
+$auth = 'https://efaturatest.izibiz.com.tr/AuthenticationWS?wsdl';
 $authentication = 'https://efaturatest.izibiz.com.tr/AuthenticationWS?wsdl';
 
 $eZrapor = 'https://efaturatest.izibiz.com.tr/ZReportWS?wsdl';
