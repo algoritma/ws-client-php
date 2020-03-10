c3f4aaeffc8bb7dec6e82e0f161e3035717db2ca
algoritma <aliihsanbayir@gmail.com>
2020-03-10 10:51:32 +0300

    Update servisLinkleri.php

--- servisLinkleri.php
+++ servisLinkleri.php
@@ -1,14 +1,24 @@
 <?php
-error_reporting(0);
-
-$eArsiv = 'https://efaturadev.izibiz.com.tr:443/EIArchiveWS/EFaturaArchive?wsdl';
+error_reporting(1);
 
 $eFatura = 'https://efaturadev.izibiz.com.tr:443/EFaturaOIB?wsdl';
 
+$eArsiv = 'https://efaturadev.izibiz.com.tr:443/EIArchiveWS/EFaturaArchive?wsdl';
+
 $eDespatch = 'https://efaturadev.izibiz.com.tr:443/EIrsaliyeWS/EIrsaliye?wsdl';
+$eIrsaliye = 'https://efaturadev.izibiz.com.tr:443/EIrsaliyeWS/EIrsaliye?wsdl';
+
+$eMustahsil = 'http://efaturatest.izibiz.com.tr/CreditNoteWS/CreditNote?wsdl';
+
+$eMutabakat = 'http://efaturatest.izibiz.com.tr/Reconciliation?wsdl';
 
 $eSmm = 'https://efaturadev.izibiz.com.tr:443/SmmWS?wsdl';
 
 $auth = 'https://efaturatest.izibiz.com.tr:443/AuthenticationWS?wsdl';
+$authentication = 'https://efaturatest.izibiz.com.tr/AuthenticationWS?wsdl';
+
+$eZrapor = 'https://efaturatest.izibiz.com.tr/ZReportWS?wsdl';
+
+$eFaturalama = 'https://efaturatest.izibiz.com.tr/BillingWS?wsdl';
 
 ?>
